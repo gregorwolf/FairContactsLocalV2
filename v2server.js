a8492956eeed593ff6df34957e1595857cd0b3fe
Gregor Wolf <gregor.wolf@gmail.com>
2020-02-19 15:13:23 +0100

    Correct v2 server from project root

--- v2server.js
+++ v2server.js
@@ -1,11 +1,8 @@
-"use strict";
-
 const express = require("express");
 const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
-const csn = './gen/srv/srv/csn.json';
 
-const host = process.env.HOST || "0.0.0.0";
+const host = "0.0.0.0";
 const port = process.env.PORT || 4004;
 
 (async () => {
@@ -14,22 +11,14 @@ const port = process.env.PORT || 4004;
   // serve odata v4
   await cds
     .connect("db") // ensure database is connected!
-    
-    await cds
-    .serve('FairContactsService')
-    .from(csn)
-    // .with('srv/service.js')
-    .in(app)
-
-
+    .serve("all")
+    .in(app);
   // serve odata v2
   process.env.XS_APP_LOG_LEVEL = "warning";
   app.use(proxy({
     path: "v2",
-    model: csn,
-    port: port, // target port
+    port: port
   }));
-
   // start server
   const server = app.listen(port, host, () => console.info(`app is listing at ${host}:${port}`));
   server.on("error", error => console.error(error.stack));
